6aef785119e60cc628136bc5c00befb6b7f219a8
Avdi Grimm <avdi@avdi.org>
2015-05-31 13:56:12 -0400

    Rewrite top-level spec to use more relatable terminology.

--- spec/brainguy_spec.rb
+++ spec/brainguy_spec.rb
@@ -2,156 +2,156 @@ require "rspec"
 require "brainguy"
 
 module Brainguy
-  class SatelliteOfLove
+  class AuctionBid
     include Eventful
 
-    def send_the_movie
-      events.emit(:movie_sign)
+    def reject_bid
+      events.emit(:rejected)
     end
 
-    def send_final_sacrifice
-      events.emit(:movie_sign, origin: "Canada", hero: "Zap Rowsdower")
+    def make_counter_offer
+      events.emit(:rejected, counter_amount: 101, valid_for_minutes: 60)
     end
 
-    def disconnect
-      events.emit(:power_out)
+    def accept_bid
+      events.emit(:accepted)
     end
   end
 
   RSpec.describe Brainguy do
     it "enables objects to publish simple events" do
       mike = spy("mike")
-      sol  = SatelliteOfLove.new
-      sol.events.on(:movie_sign) do
-        mike.panic!
+      bid  = AuctionBid.new
+      bid.events.on(:rejected) do
+        mike.handle_rejection
       end
-      sol.send_the_movie
-      expect(mike).to have_received(:panic!)
+      bid.reject_bid
+      expect(mike).to have_received(:handle_rejection)
     end
 
     it "allows multiple listeners to subscribe" do
       mike = spy("mike")
       crow = spy("crow")
-      sol  = SatelliteOfLove.new
-      sol.events.on(:movie_sign) do
-        mike.panic!
+      bid  = AuctionBid.new
+      bid.events.on(:rejected) do
+        mike.handle_rejection
       end
-      sol.events.on(:movie_sign) do
-        crow.also_panic!
+      bid.events.on(:rejected) do
+        crow.handle_rejection
       end
-      sol.send_the_movie
-      expect(mike).to have_received(:panic!)
-      expect(crow).to have_received(:also_panic!)
+      bid.reject_bid
+      expect(mike).to have_received(:handle_rejection)
+      expect(crow).to have_received(:handle_rejection)
     end
 
     it "allows multiple events to be emitted" do
       crow = spy("crow")
-      sol  = SatelliteOfLove.new
-      sol.events.on(:movie_sign) do
-        crow.panic
+      bid  = AuctionBid.new
+      bid.events.on(:rejected) do
+        crow.handle_rejection
       end
-      sol.events.on(:power_out) do
-        crow.loot
+      bid.events.on(:accepted) do
+        crow.handle_acceptance
       end
-      sol.send_the_movie
-      expect(crow).to have_received(:panic)
-      expect(crow).to_not have_received(:loot)
-      sol.disconnect
-      expect(crow).to have_received(:loot)
+      bid.reject_bid
+      expect(crow).to have_received(:handle_rejection)
+      expect(crow).to_not have_received(:handle_acceptance)
+      bid.accept_bid
+      expect(crow).to have_received(:handle_acceptance)
     end
 
     it "allows for zero subscribers" do
-      sol = SatelliteOfLove.new
-      expect { sol.send_the_movie }.to_not raise_error
+      bid = AuctionBid.new
+      expect { bid.reject_bid }.to_not raise_error
     end
 
     it "passes the event emitting object to handler blocks" do
-      sol = SatelliteOfLove.new
+      bid = AuctionBid.new
       expect do |b|
-        sol.events.on(:movie_sign, &b)
-        sol.send_the_movie
-      end.to yield_with_args(sol, anything)
+        bid.events.on(:rejected, &b)
+        bid.reject_bid
+      end.to yield_with_args(bid, anything)
       expect do |b|
-        sol.events.on(:power_out, &b)
-        sol.disconnect
-      end.to yield_with_args(sol, anything)
+        bid.events.on(:accepted, &b)
+        bid.accept_bid
+      end.to yield_with_args(bid, anything)
     end
 
     it "passes the event name to handler blocks" do
-      sol = SatelliteOfLove.new
+      bid = AuctionBid.new
       expect do |b|
-        sol.events.on(:movie_sign, &b)
-        sol.send_the_movie
-      end.to yield_with_args(anything, :movie_sign)
+        bid.events.on(:rejected, &b)
+        bid.reject_bid
+      end.to yield_with_args(anything, :rejected)
     end
 
-    it "passes extra emit args on to the handler block" do
-      sol        = SatelliteOfLove.new
-      movie_info = :not_set
-      sol.events.on(:movie_sign) do |object, event, info|
-        movie_info = info
+    it "passes extra event args on to the handler block" do
+      bid        = AuctionBid.new
+      counter_info = :not_set
+      bid.events.on(:rejected) do |object, event, info|
+        counter_info = info
       end
-      sol.send_final_sacrifice
-      expect(movie_info[:origin]).to eq("Canada")
-      expect(movie_info[:hero]).to eq("Zap Rowsdower")
+      bid.make_counter_offer
+      expect(counter_info[:counter_amount]).to eq(101)
+      expect(counter_info[:valid_for_minutes]).to eq(60)
     end
 
     it "allows a subscription to be removed" do
-      sol = SatelliteOfLove.new
+      bid = AuctionBid.new
       expect do |b|
-        subscription = sol.events.on(:movie_sign, &b)
+        subscription = bid.events.on(:rejected, &b)
         subscription.cancel
-        sol.send_the_movie
+        bid.reject_bid
       end.to_not yield_control
     end
 
     it "allows an object to listen to all events" do
-      sol      = SatelliteOfLove.new
+      bid      = AuctionBid.new
       listener = spy("listener")
-      sol.events.subscribe(listener)
+      bid.events.subscribe(listener)
 
-      sol.send_the_movie
-      expect(listener).to have_received(:call).with(sol, :movie_sign)
+      bid.reject_bid
+      expect(listener).to have_received(:call).with(bid, :rejected)
 
-      sol.disconnect
-      expect(listener).to have_received(:call).with(sol, :power_out)
+      bid.accept_bid
+      expect(listener).to have_received(:call).with(bid, :accepted)
     end
 
     it "allows a listener to be unsubscribed" do
-      sol      = SatelliteOfLove.new
+      bid      = AuctionBid.new
       listener = spy("listener")
-      subscription = sol.events.subscribe(listener)
+      subscription = bid.events.subscribe(listener)
 
-      sol.send_the_movie
-      expect(listener).to have_received(:call).with(sol, :movie_sign)
+      bid.reject_bid
+      expect(listener).to have_received(:call).with(bid, :rejected)
 
       subscription.cancel
 
-      sol.disconnect
-      expect(listener).not_to have_received(:call).with(sol, :power_out)
+      bid.accept_bid
+      expect(listener).not_to have_received(:call).with(bid, :accepted)
     end
 
     it "allows a listener to be unsubscribed by identity" do
-      sol      = SatelliteOfLove.new
+      bid      = AuctionBid.new
       listener = spy("listener")
-      sol.events.subscribe(listener)
+      bid.events.subscribe(listener)
 
-      sol.send_the_movie
-      expect(listener).to have_received(:call).with(sol, :movie_sign)
+      bid.reject_bid
+      expect(listener).to have_received(:call).with(bid, :rejected)
 
-      sol.events.unsubscribe(listener)
-      expect(sol.events).to be_empty
+      bid.events.unsubscribe(listener)
+      expect(bid.events).to be_empty
 
-      sol.disconnect
-      expect(listener).not_to have_received(:call).with(sol, :power_out)
+      bid.accept_bid
+      expect(listener).not_to have_received(:call).with(bid, :accepted)
     end
 
     it "does not allow the same listener to be subscribed twice" do
-      sol      = SatelliteOfLove.new
+      bid      = AuctionBid.new
       listener = spy("listener")
-      sol.events.subscribe(listener)
-      sol.events.subscribe(listener)
-      sol.send_the_movie
+      bid.events.subscribe(listener)
+      bid.events.subscribe(listener)
+      bid.reject_bid
       expect(listener).to have_received(:call).once
     end
   end
